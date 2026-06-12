b20f2ab695fd1e6fda17c9423ffe4879c413fb23
Bilal Raza <bilalraza06000@gmail.com>
2026-06-12 11:18:06 +0500

    refactor(delete-confirmation): replace mat-icon with inline SVGs

--- projects/osl-base-extended/src/lib/core/shared/components/delete-confirmation/delete-confirmation.ts
+++ projects/osl-base-extended/src/lib/core/shared/components/delete-confirmation/delete-confirmation.ts
@@ -1,11 +1,10 @@
 import { Component, Inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
-import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'delete-confirmation',
-  imports: [MatDialogModule, MatIconModule, MatButtonModule],
+  imports: [MatDialogModule, MatButtonModule],
   templateUrl: './delete-confirmation.html',
   styleUrl: './delete-confirmation.scss',
 })
